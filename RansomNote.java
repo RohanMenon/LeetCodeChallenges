46612eed6e18f58e5c86134397d97a44aff22b58
Rohan Menon <menon.rohan96@gmail.com>
2020-05-03 16:11:22 +0530

    Problem #3
    Added problem statement #3 and solution

---
+++ RansomNote.java
@@ -0,0 +1,37 @@
+/*Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+*/
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        int[] ransomFrequency = new int[26];
+        int[] magazineFrequency = new int[26];
+        boolean result=true;
+        
+        for(int i=0;i<26;i++) ransomFrequency[i]=0;
+        
+        for(int i=0;i<26;i++) magazineFrequency[i]=0;
+        
+        for(int i=0;i<ransomNote.length();i++) ransomFrequency[ransomNote.charAt(i)-'a'] += 1; 
+        
+        for(int i=0;i<magazine.length();i++) magazineFrequency[magazine.charAt(i)-'a'] += 1;  
+        
+        for(int i=0;i<26;i++){
+            if(ransomFrequency[i]!=0 && (ransomFrequency[i] > magazineFrequency[i])){
+                result = false;
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
