d6902f6592d6c01c1b2def0ca28b68c664a57d83
Rohan Menon <menon.rohan96@gmail.com>
2020-05-14 23:48:56 +0530

    Update 4_NumberComplement.java

--- May/4_NumberComplement.java
+++ May/4_NumberComplement.java
@@ -1,4 +1,5 @@
-/*Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+/*
+Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
 
 Example 1:
 
@@ -18,7 +19,9 @@ Note:
 The given integer is guaranteed to fit within the range of a 32-bit signed integer.
 You could assume no leading zero bit in the integer’s binary representation.
 This question is the same as 1009: https://leetcode.com/problems/complement-of-base-10-integer/
+
 */
+
 
 class Solution {
     public int findComplement(int num) {
