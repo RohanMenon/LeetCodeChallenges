2c8abbb57945309bbd99fe7ffe481527f422c1d6
Rohan Menon <menon.rohan96@gmail.com>
2020-05-14 23:47:38 +0530

    Update 1_FirstBadVersion.java

--- May/1_FirstBadVersion.java
+++ May/1_FirstBadVersion.java
@@ -1,4 +1,5 @@
-/*You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+/*
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
 Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
 
@@ -13,7 +14,9 @@ call isBadVersion(5) -> true
 call isBadVersion(4) -> true
 
 Then 4 is the first bad version. 
+
 */
+
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
