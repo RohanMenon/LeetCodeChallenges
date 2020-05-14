98d6371b148cff100697f68f4818844f1c16615c
Rohan Menon <menon.rohan96@gmail.com>
2020-05-14 23:48:28 +0530

    Update 3_RansomNote.java

--- May/3_RansomNote.java
+++ May/3_RansomNote.java
@@ -1,4 +1,5 @@
-/*Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+/*
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
 
 Each letter in the magazine string can only be used once in your ransom note.
 
@@ -8,7 +9,9 @@ You may assume that both strings contain only lowercase letters.
 canConstruct("a", "b") -> false
 canConstruct("aa", "ab") -> false
 canConstruct("aa", "aab") -> true
+
 */
+
 
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
