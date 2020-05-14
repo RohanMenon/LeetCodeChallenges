f91b9464230aaaf007ff8b41872464378a46eb04
Rohan Menon <menon.rohan96@gmail.com>
2020-05-14 23:49:20 +0530

    Update 5_FirstUniqueCharacterInString.java

--- May/5_FirstUniqueCharacterInString.java
+++ May/5_FirstUniqueCharacterInString.java
@@ -1,4 +1,5 @@
-/*Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+/*
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
 
 Examples:
 
@@ -8,7 +9,9 @@ return 0.
 s = "loveleetcode",
 return 2.
 Note: You may assume the string contain only lowercase letters.
+
 */
+
 
 class Solution {
     public int firstUniqChar(String s) {
