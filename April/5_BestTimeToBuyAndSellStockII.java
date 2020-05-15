b1433700dfcbd8f2a9e0943ab1df9edb0deeaf4b
Rohan Menon <menon.rohan96@gmail.com>
2020-05-15 23:58:49 +0530

    Update 5_BestTimeToBuyAndSellStockII.java

--- April/5_BestTimeToBuyAndSellStockII.java
+++ April/5_BestTimeToBuyAndSellStockII.java
@@ -1,6 +1,5 @@
 /*
 Say you have an array prices for which the ith element is the price of a given stock on day i.
-
 Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
 
 Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
@@ -18,6 +17,7 @@ Output: 4
 Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
              Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
              engaging multiple transactions at the same time. You must sell before buying again.
+             
 Example 3:
 
 Input: [7,6,4,3,1]
@@ -28,6 +28,7 @@ Constraints:
 
 1 <= prices.length <= 3 * 10 ^ 4
 0 <= prices[i] <= 10 ^ 4
+
 
 */
 
