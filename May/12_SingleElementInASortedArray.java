a565ecaf7b70aae4eac6e9384b8e8c9b3bd351a1
Rohan Menon <menon.rohan96@gmail.com>
2020-05-16 23:55:57 +0530

    Update 12_SingleElementInASortedArray.java

--- May/12_SingleElementInASortedArray.java
+++ May/12_SingleElementInASortedArray.java
@@ -1,7 +1,6 @@
 /*
 You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
 
-
 Example 1:
 
 Input: [1,1,2,3,3,4,4,8,8]
