d26fe9a9ed792e67c61a6d6d216f5d0895f901c4
Rohan Menon <menon.rohan96@gmail.com>
2020-05-20 23:50:34 +0530

    Update 4_MoveZeroes.java

--- April/4_MoveZeroes.java
+++ April/4_MoveZeroes.java
@@ -16,12 +16,8 @@ Minimize the total number of operations.
 class Solution {
     public void moveZeroes(int[] nums) {
         int count = -1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] != 0)
-                nums[++count] = nums[i];;
-        }
+        for(int i=0;i<nums.length;i++) if(nums[i] != 0) nums[++count] = nums[i];
         
-        for(int i = count+1;i<nums.length;i++)
-            nums[i] = 0;
+        for(int i = count+1;i<nums.length;i++) nums[i] = 0;
     }
 }
