9028e2cbe0288bd8d9c6986c42f94fc306b9a86d
Rohan Menon <menon.rohan96@gmail.com>
2020-05-29 00:51:35 +0530

    Problem #28

---
+++ May/Week 4/28_CountingBits.java
@@ -0,0 +1,41 @@
+/*
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example 1:
+
+Input: 2
+Output: [0,1,1]
+Example 2:
+
+Input: 5
+Output: [0,1,1,2,1,2]
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+
+*/
+
+/*Intuition can be found here: http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetTable */
+
+class Solution {
+    static int[] bitsLookUpTable = new int[256];
+    
+    public int[] countBits(int num) {        
+        bitsLookUpTable[0] = 0;
+        for(int i=0;i<256;i++)
+            bitsLookUpTable[i] = (i & 1) + bitsLookUpTable[i/2];
+        
+        List<Integer> setBitCount = new ArrayList<>();
+        int count = 0;    
+        for(int i=0;i<=num;i++){
+            count = bitsLookUpTable[i & 0xff] + bitsLookUpTable[(i >> 8) & 0xff] 
+                + bitsLookUpTable[(i >> 16) & 0xff] + bitsLookUpTable[(i >> 24) & 0xff];
+                   
+            setBitCount.add(count);
+        }
+        
+        return setBitCount.stream().mapToInt(k->k).toArray();
+    }
+}
